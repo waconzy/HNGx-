1ad6e968aac56f40eb7d82efb0d67a5807bf6b91
olawale olatona <81704184+waconzy@users.noreply.github.com>
2023-09-15 13:00:25 +0100

    Update DetailsBanner.jsx

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -67,9 +67,7 @@ const DetailsBanner = ({ video, crew }) => {
                                         <div className="title" data-testid='movie-title'>
                                             {`${
                                                 data.name || data.title
-                                            } (${dayjs(
-                                                data?.release_date
-                                            ).format("YYYY")})`}
+                                            } 
                                         </div>
                                         <div className="subtitle">
                                             {data.tagline}
